855114d65d00a739e2f9ec74537a4cf5c5a05c55
tnail45 <tjnail2000@gmail.com>
2024-09-10 08:43:14 -0700

    Fix typo

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ const equalButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const prevOperand = document.querySelector('[data-previous-operand]');
-const currOperand = document.querySelector('[data-corrent-opera nd]');
+const currOperand = document.querySelector('[data-current-operand]');
