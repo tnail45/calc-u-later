3c3842858bf5923660815a3b4c4dc556eca77c3f
tnail45 <tjnail2000@gmail.com>
2024-09-10 08:34:43 -0700

    Create initial variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const numButton = document.querySelectorAll('[data-number]');
+const operatorButton = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const prevOperand = document.querySelector('[data-previous-operand]');
+const currOperand = document.querySelector('[data-corrent-opera nd]');
